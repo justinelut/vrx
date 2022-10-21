f5db59805e29f9928effece5c35dd8aa8c337215
justine/lut <justinequartz@gmail.com>
2022-10-21 16:10:34 +0300

    Publishing branch

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const port = 3000
 
 
 
-  // cron.schedule('*/30 * * * *', () => {
-  //   ScrapData('https://www.tuko.co.ke/', 'article a', 'article h1', 'article .post__content', 'article .article-image .article-image__picture')
-  //   console.log('Running a web scrap of tuko news');
-  // }, {
-  //   scheduled: true,
-  //   timezone: "Africa/Nairobi"
-  // });
+  cron.schedule('*/30 * * * *', () => {
+    ScrapData('https://www.tuko.co.ke/', 'article a', 'article h1', 'article .post__content', 'article .article-image .article-image__picture')
+    console.log('Running a web scrap of tuko news');
+  }, {
+    scheduled: true,
+    timezone: "Africa/Nairobi"
+  });
 
   //football
-  cron.schedule('*/1 * * * *', () => {
+  cron.schedule('*/59 * * * *', () => {
     scrapFootball('https://www.football.london/', '.teaser-text a', 'article div h1', '.article-wrapper p', 'article .article-wrapper .content-column figure .img-container img')
     console.log('Running a web scrap of football.london');
   }, {
